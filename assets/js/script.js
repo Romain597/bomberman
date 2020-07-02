0807f1b4d5d118a8687b5d8080d9a7ab5bebed1f
Romain Balestreri <romain.balestreri@orange.fr>
2020-07-02 15:37:58 +0200

    update hit

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,10 +23,8 @@ window.onload = () => {
     const explosionClass = "explosion";
     const bombBlinkClass = "bomb-blink"
     const bombBlinkInterval = 500;
-    //const indexX = 1;
-    //const indexY = 0;
+    const sameHit = false;
     let inGame = false;
-    //let playerPos = [initialY,initialX];
     let bombCount = bombMaxCount;
     let lifeCount = lifeMaxCount;
     let foesCount = foesMaxCount;
@@ -34,10 +32,16 @@ window.onload = () => {
     let bombsArray = [];
     let timeoutArray = [];
     let intervalArray = [];
-    //let hitsArray = [];
+    let hitsArray = [];
 
-    // bug : life count -1 quand bombe boom
-
+    function addZero(val, n) {
+        if(isNaN(val)==false) {
+            while(val.toString().length < n) {
+                val = "0" + val;
+            }
+        }
+        return val;
+    }
     function getPos(object,property) {
         return parseInt(window.getComputedStyle(object).getPropertyValue(property).replace("px",""));
     }
@@ -100,7 +104,6 @@ window.onload = () => {
             let scale = 1+(explosionRadius*2);
             bomb.style.transform = "scale("+scale+")";
             let foesInRadiusA = detectFoesInRadius(valY,valX);
-            //let playerInRadius = false;
             let playerInRadiusA = detectPlayerInRadius(valY,valX);
             if(playerInRadiusA) {
                 collisionAction(true);
@@ -110,9 +113,6 @@ window.onload = () => {
                 let playerInRadiusB = detectPlayerInRadius(valY,valX);
                 let foesInRadius = foesInRadiusA;
                 foesInRadiusB.forEach(item => foesInRadius.includes(item) ? null : foesInRadius.push(item));
-                /*if(playerInRadiusA==true || playerInRadiusB==true) {
-                    playerInRadius = true;
-                }*/
                 deleteFoesInRadius(foesInRadius);
                 bomb.remove();
                 bombsArray.splice(indexInArray,1);
@@ -162,15 +162,21 @@ window.onload = () => {
             let tempX = getPos(foe,"left");
             if(tempY==playerPos[0] && tempX==playerPos[1]) {
                 val = true;
+                let d = new Date(); 
+                let time = d.getTime().toString(); time = parseInt(time);
+                hitsArray.push([foe.id,time]);
             }
         }
         else {
+            let d = new Date(); 
+            let time = d.getTime().toString(); time = parseInt(time);
             foesArray.forEach(div => {
                 let tempEl = container.querySelector("#"+div[0]);
                 let tempY = getPos(tempEl,"top");
                 let tempX = getPos(tempEl,"left");
                 if(tempY==playerPos[0] && tempX==playerPos[1]) {
                     val = true;
+                    hitsArray.push([div[0],time]);
                 }
             });
         }
@@ -229,6 +235,9 @@ window.onload = () => {
         let playerPos = getPlayerPos();
         let newPosition = playerPos;
         if(lifeCount>=0) {
+            let hitDown = true;
+            let d = new Date();
+            let time = d.getTime().toString(); time = parseInt(time);
             switch(keyCode) {
                 case "ArrowUp":
                     if(playerPos[0]>=offset) {
@@ -236,8 +245,13 @@ window.onload = () => {
                         player.style.top = newPosition[0]+"px";
                         let samePos = testCollision();
                         if(samePos) {
+                            if(sameHit==false && hitsArray.length>1) {
+                                if((hitsArray[hitsArray.length-2][1]+foesDelay)>hitsArray[hitsArray.length-1][1] && hitsArray[hitsArray.length-1][0]==hitsArray[hitsArray.length-2][0]) {
+                                    hitDown = false;
+                                }
+                            }
                             if(lifeCount>0) {
-                                collisionAction(true);
+                                collisionAction(hitDown);
                             }
                             else {
                                 gameFail();
@@ -251,8 +265,13 @@ window.onload = () => {
                         player.style.top = newPosition[0]+"px";
                         let samePos = testCollision();
                         if(samePos) {
+                            if(sameHit==false && hitsArray.length>1) {
+                                if((hitsArray[hitsArray.length-2][1]+foesDelay)>hitsArray[hitsArray.length-1][1] && hitsArray[hitsArray.length-1][0]==hitsArray[hitsArray.length-2][0]) {
+                                    hitDown = false;
+                                }
+                            }
                             if(lifeCount>0) {
-                                collisionAction(true);
+                                collisionAction(hitDown);
                             }
                             else {
                                 gameFail();
@@ -266,8 +285,13 @@ window.onload = () => {
                         player.style.left = newPosition[1]+"px";
                         let samePos = testCollision();
                         if(samePos) {
+                            if(sameHit==false && hitsArray.length>1) {
+                                if((hitsArray[hitsArray.length-2][1]+foesDelay)>hitsArray[hitsArray.length-1][1] && hitsArray[hitsArray.length-1][0]==hitsArray[hitsArray.length-2][0]) {
+                                    hitDown = false;
+                                }
+                            }
                             if(lifeCount>0) {
-                                collisionAction(true);
+                                collisionAction(hitDown);
                             }
                             else {
                                 gameFail();
@@ -281,8 +305,13 @@ window.onload = () => {
                         player.style.left = newPosition[1]+"px";
                         let samePos = testCollision();
                         if(samePos) {
+                            if(sameHit==false && hitsArray.length>1) {
+                                if((hitsArray[hitsArray.length-2][1]+foesDelay)>hitsArray[hitsArray.length-1][1] && hitsArray[hitsArray.length-1][0]==hitsArray[hitsArray.length-2][0]) {
+                                    hitDown = false;
+                                }
+                            }
                             if(lifeCount>0) {
-                                collisionAction(true);
+                                collisionAction(hitDown);
                             }
                             else {
                                 gameFail();
@@ -431,8 +460,14 @@ window.onload = () => {
                 el.style.left = (valX+offsetX)+"px";
                 let samePos = testCollision(el);
                 if(samePos) {
+                    let hitDown = true;
+                    if(sameHit==false && hitsArray.length>1) {
+                        if((hitsArray[hitsArray.length-2][1]+foesDelay)>hitsArray[hitsArray.length-1][1] && div[0]==hitsArray[hitsArray.length-2][0]) {
+                            hitDown = false;
+                        }
+                    }
                     if(lifeCount>0) {
-                        collisionAction(true);
+                        collisionAction(hitDown);
                     }
                     else {
                         gameFail();
@@ -454,7 +489,6 @@ window.onload = () => {
         foesArray = [];
         bombsArray = [];
         timeoutArray = [];
-        //playerPos = [initialY,initialX];
         player.style.top = initialY+"px";
         player.style.left = initialX+"px";
         lifeNbElement.innerText = lifeCount;
@@ -483,7 +517,6 @@ window.onload = () => {
     }
 
     window.addEventListener("keyup", event => {
-        //console.log(event.code); console.log(inGame);
         if(event.code=="Digit0" || event.code=="Numpad0") {
             startGame();
         }
